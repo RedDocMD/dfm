2ece7b2982cb7901e8df61562175a07b1735fad9
Deep Majumder <deep.majumder2019@gmail.com>
2022-08-13 18:47:51 +0530

    Renamed AppState to PaneState, implemented directory changing

--- src/State.hs
+++ src/State.hs
@@ -1,14 +1,20 @@
 module State
-    ( AppState(AppState)
+    ( PaneState(PaneState)
     , highlightNextFile
     , highlightPrevFile
     , highlightedFile
+    , enterHighlightedFile
     ) where
 
-import           System.FilePath
+
+import           System.Directory               ( doesDirectoryExist
+                                                , getDirectoryContents
+                                                )
+import           System.FilePath                ( (</>) )
+
 
 -- Contains the current state of the app
-data AppState = AppState
+data PaneState = PaneState
     { mainPath           :: FilePath
     , pathFiles          :: [FilePath]
     , highlightedFileIdx :: Int
@@ -17,19 +23,35 @@ data AppState = AppState
 
 
 -- Highlight the next file
-highlightNextFile :: AppState -> AppState
+highlightNextFile :: PaneState -> PaneState
 highlightNextFile st = st
     { highlightedFileIdx = nextFileIdx (highlightedFileIdx st)
     }
     where nextFileIdx idx = min (length $ pathFiles st) (idx + 1)
 
 -- Highlight the prev file
-highlightPrevFile :: AppState -> AppState
+highlightPrevFile :: PaneState -> PaneState
 highlightPrevFile st = st
     { highlightedFileIdx = prevFileIdx (highlightedFileIdx st)
     }
     where prevFileIdx idx = max 0 (idx - 1)
 
 -- Get current highlighted file
-highlightedFile :: AppState -> FilePath
+highlightedFile :: PaneState -> FilePath
 highlightedFile st = mainPath st </> (pathFiles st !! highlightedFileIdx st)
+
+-- If the highlighted file is a directory, then that becomes the
+-- main path. Otherwise, nothing happens.
+enterHighlightedFile :: PaneState -> IO PaneState
+enterHighlightedFile st = do
+    let currPath = highlightedFile st
+        newSt    = do
+            contents <- getDirectoryContents currPath
+            return
+                (st { mainPath           = currPath
+                    , pathFiles          = contents
+                    , highlightedFileIdx = 0
+                    }
+                )
+    isDir <- doesDirectoryExist currPath
+    if isDir then return st else newSt
