d735a49f8a842cdf56d79fed40f9d8bb44a54edd
Deep Majumder <deep.majumder2019@gmail.com>
2022-08-16 16:02:09 +0530

    Added a FSEntry data type to represent a file name with its Unix file type

---
+++ src/FS.hs
@@ -0,0 +1,60 @@
+module FS
+    ( FSEntry(..)
+    , isHiddenFile
+    , listFSEntry
+    , FileType(..)
+    ) where
+
+import           Data.List.Extra
+import           System.Directory               ( listDirectory )
+import           System.FilePath
+import           System.Posix.Files
+
+data FileType = File | Directory | BlockDevice | CharDevice | NamedPipe | Socket deriving (Show, Eq)
+
+data FSEntry = FSEntry
+    { name      :: FilePath
+    , fileType  :: FileType
+    , isSymLink :: Bool
+    }
+    deriving Show
+
+instance Ord FSEntry where
+    l <= r = lower (name l) <= lower (name r)
+
+instance Eq FSEntry where
+    l == r = name l == name r
+
+isHiddenFile :: FSEntry -> Bool
+isHiddenFile = startsDot . name
+  where
+    startsDot []      = False
+    startsDot (x : _) = x == '.'
+
+listFSEntry :: FilePath -> IO [FSEntry]
+listFSEntry fp = do
+    subEntries <- listDirectory fp
+    let subPaths = map (fp </>) subEntries
+    subTypes <- mapM fsType subPaths
+    symLinks <- mapM isPathSymLink subPaths
+    let entries = zipWith3
+            (\nm t sl -> FSEntry { name = nm, fileType = t, isSymLink = sl })
+            subEntries
+            subTypes
+            symLinks
+    return entries
+
+fsType :: FilePath -> IO FileType
+fsType path = getFileStatus path >>= \x -> return $ fileStatusToType x
+
+isPathSymLink :: FilePath -> IO Bool
+isPathSymLink path = getFileStatus path >>= \x -> return $ isSymbolicLink x
+
+fileStatusToType :: FileStatus -> FileType
+fileStatusToType fs | isBlockDevice fs     = BlockDevice
+                    | isCharacterDevice fs = CharDevice
+                    | isNamedPipe fs       = NamedPipe
+                    | isRegularFile fs     = File
+                    | isDirectory fs       = Directory
+                    | otherwise            = error "Unknown file type"
+
