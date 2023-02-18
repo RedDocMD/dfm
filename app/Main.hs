5924db52b1669d87cb7f0ab79ec326b0736376b1
Deep Majumder <deep.majumder2019@gmail.com>
2023-02-18 10:29:01 +0530

    Added logging to track slow events

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main
     ( main
     ) where
 
-import           Graphics.Vty
-import           Lib
-import           State
+import           Control.Logging                ( withFileLogging )
+import qualified Control.Logging               as Log
+import           Data.Text                      ( pack )
+import           Graphics.Vty                   ( Config
+                                                , mkVty
+                                                , nextEvent
+                                                , picForImage
+                                                , shutdown
+                                                , standardIOConfig
+                                                , update
+                                                )
+import           Lib                            ( renderState
+                                                , updateState
+                                                )
+import           State                          ( AppState
+                                                , defaultAppState
+                                                )
 
 main :: IO ()
-main = do
+main = withFileLogging "/tmp/dfm.log" $ do
     cfg <- standardIOConfig
     as  <- defaultAppState
     mainLoop cfg as
 
 mainLoop :: Config -> AppState -> IO ()
 mainLoop cfg as = do
+    Log.log "At the beginning of new loop"
     vty <- mkVty cfg
     img <- renderState cfg as
     update vty $ picForImage img
-    ev            <- nextEvent vty
+    Log.log "Waiting for event"
+    ev <- nextEvent vty
+    Log.log (pack $ show ev)
     (newAs, quit) <- updateState cfg as ev
     if quit then shutdown vty else mainLoop cfg newAs
